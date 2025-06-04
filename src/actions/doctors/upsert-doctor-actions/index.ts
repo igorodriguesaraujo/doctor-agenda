cedceba82da78c6aba6c2e91b789d4e4aaffbb9d
igorodriguesaraujo <igorodrigues_@hotmail.com>
2025-06-04 00:58:47 -0300

    feat: Adicionar server action de upsert para médicos com validação de sessão e conversão de horários para UTC

---
+++ src/actions/doctors/upsert-doctor-actions/index.ts
@@ -0,0 +1,66 @@
+'use server'
+
+import dayjs from 'dayjs'
+import utc from "dayjs/plugin/utc";
+import { headers } from "next/headers";
+
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/drizzle";
+import { upsertDoctorSchema } from "./schema";
+import { doctorsTable } from "@/database/schema";
+import { actionClient } from "@/lib/next-safe-action";
+
+dayjs.extend(utc);
+
+export const upsertDoctorAction = actionClient
+  .schema(upsertDoctorSchema)
+  .action(async ({ parsedInput }) => {
+    const availableFromTime = parsedInput.availableFromTime; // 15:30:00
+    const availableToTime = parsedInput.availableToTime; // 16:00:00
+
+    const availableFromTimeUTC = dayjs()
+      .set("hour", parseInt(availableFromTime.split(":")[0]))
+      .set("minute", parseInt(availableFromTime.split(":")[1]))
+      .set("second", parseInt(availableFromTime.split(":")[2]))
+      .utc();
+    const availableToTimeUTC = dayjs()
+      .set("hour", parseInt(availableToTime.split(":")[0]))
+      .set("minute", parseInt(availableToTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
+    const session = await auth.api.getSession({
+      headers: await headers()
+    })
+
+    if (!session?.user) {
+      throw new Error('Usuário não autorizado.')
+    }
+
+    if (!session?.user?.clinic?.id) {
+      throw new Error('Clínica não encontrada.')
+    }
+
+    const timeTo = availableToTimeUTC.format("HH:mm:ss");
+    const timeFrom = availableFromTimeUTC.format("HH:mm:ss");
+    const appointmentPriceInCents = parsedInput.appointmentPrice * 100;
+
+    await db
+      .insert(doctorsTable)
+      .values({
+        ...parsedInput,
+        availableToTime: timeTo,
+        availableFromTime: timeFrom,
+        clinicId: session.user.clinic.id,
+        appointmentPriceInCents: appointmentPriceInCents,
+      })
+      .onConflictDoUpdate({
+        target: [doctorsTable.id],
+        set: {
+          ...parsedInput,
+          availableFromTime: timeFrom,
+          availableToTime: timeTo,
+          appointmentPriceInCents: appointmentPriceInCents,
+        }
+      })
+  })
